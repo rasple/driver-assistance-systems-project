8170888a6a7de5a260cede0f2084fe18f20b2485
simon-frank <simon.frank98@web.de>
2019-04-09 15:29:53 +0200

    add const r

--- plot4_6_1.m
+++ plot4_6_1.m
@@ -1,19 +1,30 @@
 function plot4_6_1()
-%Ch, Cv, lv, lh, m, g
+    %Ch, Cv, lv, lh, m, g
     %ChStern = Ch * (lv+lh) / lv / m / g;
     %CvStern = Cv * (lv+lh) / lh / m / g;
+    %konstante geschwinidkeit
     Cvs=9.5;
     Chs=20.5;
     g=9.81;
-    EG=((1/Cvs)-(1/Chs))/g
-    R=10;
+    EG=((1/Cvs)-(1/Chs))/g;
+    %konstante geschwinigkeit
     v=20;
     l=5;
     v_2=v*v;
-    r=30:0.01:50 %DIN/ISO4138
+    r=1000:0.01:4000; %DIN/ISO4138
     a=v_2./r;
     ack=l./r;
-    oben=ack+EG;
-    unten=ack-EG;
+    oben=ack+(EG.*a);
+    unten=ack-(EG.*a);
+    subplot(2,1,1);
     plot(a, ack,'--', a, oben, a, unten);
+    %konstante Radius
+    v=0:0.01:7;
+    R=100;
+    a= (v.^2)/R;
+    ack = (l/R) * ones(1, length(v));
+    oben=ack+(EG.*a);
+    unten=ack-(EG.*a);
+    subplot(2,1,2);
+    plot(a, ack, a, oben, a, unten);
 end
