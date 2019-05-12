9b8cb0744a7de4f13f56cbac121fbeae04fd8bd9
simon-frank <simon.frank98@web.de>
2019-05-12 15:43:58 +0200

    change plot

--- plot3_7.m
+++ plot3_7.m
@@ -8,21 +8,10 @@ function plot3_7(Gg,Gh, l, hs)
 %plot erster Quadrant
 x=hs/l;
 psi=Gh/Gg;
-z=[0:0.01:(psi/x)];
-f1= z.* (psi-z.*x);
-subplot(2,1,1);
-plot(z,f1);
-title('Erster Quadrant')
-str_x = '$$ \frac{ F_{ Bv } }{ G_{g} } $$';
-xlabel(str_x,'Interpreter','latex')
-str_y = '$$ \frac{ F_{ Bh } }{ G_{g} } $$';
-ylabel(str_y,'Interpreter','latex')
-%plot dritter Quadrant
-subplot(2,1,2)
-z=[(-(1-psi)/x):0.01:0]
-f3= z.*(1-psi+z.*x);
-plot(z,f3);
-title('Dritter Quadrant');
+z=[(-(1-psi)/x):0.01:(psi/x)];
+FBV= z.*(1-psi+z.*x);
+FBH= z.*(psi-z.*x);
+plot(FBV,FBH);
 str_y = '$$ \frac{ F_{ Bv } }{ G_{g} } $$';
 ylabel(str_y,'Interpreter','latex')
 str_x = '$$ \frac{ F_{ Bh } }{ G_{g} } $$';
