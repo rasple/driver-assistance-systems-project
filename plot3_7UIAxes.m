0ce2ae5d43588e08ba25f1d07d19a87ced649366
simon-frank <simon.frank98@web.de>
2019-05-12 15:29:44 +0200

    change 3_7

--- plot3_7UIAxes.m
+++ plot3_7UIAxes.m
@@ -4,26 +4,17 @@
 % Gh=Hinterachsgewichtskraft
 % l=Radstand
 % hs=Schwerpunkth�he
-function plot3_7UIAxes(axes1, axes2, Gg,Gh, l, hs)
+function plot3_7UIAxes(axes1, Gg,Gh, l, hs)
 %plot erster Quadrant
 x=hs/l;
 psi=Gh/Gg;
-z=[0:0.01:(psi/x)];
-f1= z.* (psi-z.*x);
-plot(axes1,f1);
-title(axes1,'Erster Quadrant')
+z=[(-(1-psi)/x):0.01:(psi/x)];
+FBV= z.*(1-psi+z.*x);
+FBH= z.*(psi-z.*x);
+plot(axes1,FBV,FBH);
+title(axes1,'Tangentialkraftverteilung')
 str_x = '$$ \frac{ F_{ Bv } }{ G_{g} } $$';
 xlabel(axes1, str_x,'Interpreter','latex')
 str_y = '$$ \frac{ F_{ Bh } }{ G_{g} } $$';
 ylabel(axes1, str_y,'Interpreter','latex')
-%plot dritter Quadrant
-
-z=[(-(1-psi)/x):0.01:0]
-f3= z.*(1-psi+z.*x);
-plot(axes2, z,f3);
-title(axes2, 'Dritter Quadrant');
-str_y = '$$ \frac{ F_{ Bv } }{ G_{g} } $$';
-ylabel(axes2, str_y,'Interpreter','latex')
-str_x = '$$ \frac{ F_{ Bh } }{ G_{g} } $$';
-xlabel(axes2, str_x,'Interpreter','latex')
 end
